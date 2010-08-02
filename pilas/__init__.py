4717049f668dcc277a3b0f3969a68b1becba3d0f
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-08-02 01:55:59 -0300

    creando bucle alternativo para que se pueda iniciar la biblioteca desde un interprete interactivo y la biblioteca lo detecte solo.

--- pilas/__init__.py
+++ pilas/__init__.py
@@ -1,24 +1,36 @@
 # -*- encoding: utf-8 -*-
 import sys
+import time
+
 from PySFML import sf
 
+app = 1
+event = 1
+clock = 1
 
-app = sf.RenderWindow(sf.VideoMode(640, 480), "Pilas")
-event = sf.Event()
-clock = sf.Clock()
-bg_color = sf.Color(200, 200, 200)
+
+
 
 
 def exit():
     "Termina la ejecución de la biblioteca."
-    app.Close()
     sys.exit(0)
 
 
 def loop():
     "Pone en funcionamiento el bucle principal."
+    global app
+
+    if app == 1:
+        app = sf.RenderWindow(sf.VideoMode(640, 480), "Pilas")
+
+    event = sf.Event()
+    clock = sf.Clock()
+    bg_color = sf.Color(200, 200, 200)
 
     while True:
+        time.sleep(0.1)
+
         app.Clear(bg_color)
 
         # Procesa todos los eventos.
@@ -28,3 +40,26 @@ def loop():
                     exit()
 
         app.Display()
+
+
+def loop_bg():
+    "Ejecuta el bucle de pilas en segundo plano."
+    import threading
+
+    bg = threading.Thread(target=loop)
+    bg.start()
+
+
+
+# Detecta si la biblioteca se esta ejecutando
+# desde el modo interactivo o desde un script.
+
+# Cuando inicia en modo interactivo se asegura
+# de crear la ventana dentro del mismo hilo que
+# tiene el contexto opengl.
+
+try:
+    cursor = sys.ps1
+    loop_bg()
+except AttributeError:
+    app = sf.RenderWindow(sf.VideoMode(640, 480), "Pilas")
