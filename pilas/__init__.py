1c9dadc577d23be6dbccc3afffd23cac7c757ed5
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-08-02 12:28:19 -0300

    creando el modulo actors y colocando la licencia de la biblioteca en las cabeceras de archivos.

--- pilas/__init__.py
+++ pilas/__init__.py
@@ -1,9 +1,17 @@
 # -*- encoding: utf-8 -*-
+# Pilas engine - A video game framework.
+#
+# Copyright 2010 - Hugo Ruscitti
+# License: LGPLv3 (see http://www.gnu.org/licenses/lgpl.html)
+#
+# Website - http://www.pilas-engine.com.ar
+
 import sys
 import time
 
 from PySFML import sf
 
+import actors
 
 app = 1
 event = 1
---
+++ pilas/actors.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+# Pilas engine - A video game framework.
+#
+# Copyright 2010 - Hugo Ruscitti
+# License: LGPLv3 (see http://www.gnu.org/licenses/lgpl.html)
+#
+# Website - http://www.pilas-engine.com.ar
+
+class Actor:
+
+    def __init__(self, image):
+        pass
+
