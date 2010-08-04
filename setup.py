a7bd3fdf9466843bd088ac59303415c45d58e66c
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-08-03 21:11:35 -0300

    creando el archivo setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='pilas',
+      version='0.1',
+      description='A simple to use video game framework.',
+      author='Hugo Ruscitti',
+      author_email='hugoruscitti@gmail.com',
+      packages=['pilas'],
+      #scripts=['pilas'],
+      )
+
