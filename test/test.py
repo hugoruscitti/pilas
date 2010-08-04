2cea37c621415cae71e4faf97848429190d5cba5
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-08-04 15:58:04 -0300

    Creando un test sencillo.

---
+++ test/test.py
@@ -0,0 +1,18 @@
+import random
+import unittest
+import pilas
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_window_creation(self):
+        self.assertFalse(pilas.app is None)
+
+    def testVersion(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
