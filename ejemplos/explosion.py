17950117e5441083f5a997d8904b21765e32bbcd
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-09-17 21:50:33 -0300

    fixes #64

---
+++ ejemplos/explosion.py
@@ -0,0 +1,13 @@
+import pilas
+
+imagen_explosion = pilas.imagenes.Grilla("explosion.png", 7)
+
+def crear_explosion(sender, x, y, button, signal):
+    explosion = pilas.actores.Animacion(imagen_explosion)
+    explosion.x = x
+    explosion.y = y
+    explosion.escala = 2
+
+
+pilas.eventos.click_de_mouse.connect(crear_explosion)
+pilas.ejecutar()
---
+++ pilas/actores/animacion.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+# Pilas engine - A video game framework.
+#
+# Copyright 2010 - Hugo Ruscitti
+# License: LGPLv3 (see http://www.gnu.org/licenses/lgpl.html)
+#
+# Website - http://www.pilas-engine.com.ar
+
+import pilas
+from pilas.actores import Actor
+from pilas.comportamientos import Comportamiento
+
+VELOCIDAD = 4
+
+class Animacion(Actor):
+    """Representa una animacion de una grilla de imagenes.
+
+    Este actor toma una grilla de cuadros de animacion
+    y los reproduce hasta que la animacion termina. Cuando
+    la animacion termina se elimina a si mismo.
+    """
+
+    def __init__(self, grilla):
+        Actor.__init__(self)
+        self.animacion = grilla
+        self.definir_cuadro(0)
+        self.tick = 0
+
+    def definir_cuadro(self, indice):
+        self.animacion.definir_cuadro(indice)
+        self.animacion.asignar(self)
+
+
+    def update(self):
+        if self.tick > VELOCIDAD:
+            self.tick = 0
+            ha_reiniciado = self.animacion.avanzar()
+            self.animacion.asignar(self)
+
+            # Si la animacion ha terminado se elimina de la pantalla.
+            if ha_reiniciado:
+                self.eliminar()
+        else:
+
+            self.tick += 1
