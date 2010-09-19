0474df7ed386a9c7ea1702ada3d20880efbebf0a
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-09-19 17:44:29 -0300

    agregando el sonido de explosion para la bomba.

--- ejemplos/explosion.py
+++ ejemplos/explosion.py
@@ -1,12 +1,14 @@
 import pilas
 
 imagen_explosion = pilas.imagenes.Grilla("explosion.png", 7)
+sonido_explosion = pilas.sonidos.cargar("explosion.wav")
 
 def crear_explosion(sender, x, y, button, signal):
     explosion = pilas.actores.Animacion(imagen_explosion)
     explosion.x = x
     explosion.y = y
     explosion.escala = 2
+    sonido_explosion.reproducir()
 
 
 pilas.eventos.click_de_mouse.connect(crear_explosion)
