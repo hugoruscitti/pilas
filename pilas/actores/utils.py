06f4c0aedea1d479d006ae6a7574529ffc9b3eca
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-10-25 19:32:00 -0300

    creando archivo auxiliar con toda la funcionalidad de actores basica.

---
+++ pilas/actores/utils.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+# Pilas engine - A video game framework.
+#
+# Copyright 2010 - Hugo Ruscitti
+# License: LGPLv3 (see http://www.gnu.org/licenses/lgpl.html)
+#
+# Website - http://www.pilas-engine.com.ar
+
+import pilas
+
+def ordenar_actores_por_valor_z():
+    "Ordena todos los actores para que se impriman con 'z' como criterio de orden."
+    pilas.actores.todos.sort()
+
+def insertar_como_nuevo_actor(actor):
+    "Coloca a un actor en la lista de actores a imprimir en pantalla."
+    pilas.actores.todos.append(actor)
+    
+def eliminar_un_actor(actor):
+    pilas.actores.todos.remove(actor)
+
+def eliminar_a_pilas.actores.todos():
+    a_eliminar = list(pilas.actores.todos)
+
+    for x in a_eliminar:
+        x.eliminar()
+
+def obtener_actor_en(x, y):
+    "Intenta obtener el actor mas cerca de la pantalla (z mas pequeño) en la posición (x, y)"
+
+    # Busca el objeto que colisiones ordenando en sentido inverso.
+    for sprite in pilas.actores.todos[::-1]:
+        if sprite.colisiona_con_un_punto(x, y):
+            return sprite
+
+    return None
+
