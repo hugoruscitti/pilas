cd9f2b227ec8949e8119ebd12acf4c570ec7f83d
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-12-25 23:46:11 -0300

    aumentando la version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
         name='pilas',
-        version='0.35',
+        version='0.36',
         description='A simple to use video game framework.',
         author='Hugo Ruscitti',
         author_email='hugoruscitti@gmail.com',
