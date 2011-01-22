d8fff163633c24733db244b09cff5cd5765125f3
hugoruscitti <devnull@localhost>
2011-01-22 14:42:52 -0300

    mejorando el cuadro de dialogo

--- experimental/dialogos/test.py
+++ experimental/dialogos/test.py
@@ -8,26 +8,52 @@ class Globo(pilas.actores.Actor):
         imagen = pilas.imagenes.cargar_imagen_cairo("globo.png")
 
         ancho, alto = self.lienzo.obtener_area_de_texto(texto, tamano=14)
-                
+        
         # esquina sup-izq
         self.lienzo.pintar_parte_de_imagen(imagen, 0, 0, 12, 12, 0, 0)
         
         # borde superior
-        for x in range(0, int(ancho) + 2, 12):
+        for x in range(0, int(ancho) + 12, 12):
             self.lienzo.pintar_parte_de_imagen(imagen, 12, 0, 12, 12, 12 + x, 0)
 
         # esquina sup-der
-        self.lienzo.pintar_parte_de_imagen(imagen, 100, 0, 12, 12, 12 + int(ancho), 0)       
+        self.lienzo.pintar_parte_de_imagen(imagen, 100, 0, 12, 12, 12 + int(ancho) + 12, 0)       
+
+        # centro del dialogo
+        for y in range(0, int(alto) + 12, 12):
+            # borde izquierdo
+            self.lienzo.pintar_parte_de_imagen(imagen, 0, 12, 12, 12, 0, 12 + y)
+            
+            # linea horizontal blanca, para el centro del dialogo.
+            for x in range(0, int(ancho) + 12, 12):
+                self.lienzo.pintar_parte_de_imagen(imagen, 12, 12, 12, 12, 12 + x, 12 + y)
+                
+            # borde derecho
+            self.lienzo.pintar_parte_de_imagen(imagen, 100, 12, 12, 12, 12 + int(ancho) + 12, 12 + y)       
+
+        # parte inferior
+        self.lienzo.pintar_parte_de_imagen(imagen, 0, 35, 12, 12, 0, 0 + int(alto) + 12 + 12)
+
+        # linea horizontal de la parte inferior
+        for x in range(0, int(ancho) + 12, 12):
+            self.lienzo.pintar_parte_de_imagen(imagen, 12, 35, 12, 12, 12 + x,  0 + int(alto) + 12 + 12)
+        
+        self.lienzo.pintar_parte_de_imagen(imagen, 100, 35, 12, 12, 12 + int(ancho) + 12, 0 + int(alto) + 12 + 12)
+
+        # Pico de la parte de abajo
+        self.lienzo.pintar_parte_de_imagen(imagen, 67, 35, 33, 25, int(ancho) - 12, 0 + int(alto) + 12 + 12)
+        
 
 
-        print ancho
+
         self.lienzo.definir_color(pilas.colores.negro)
         self.lienzo.escribir(texto, 12, 25, tamano=14)
         self.lienzo.asignar(self)
+        
 
 
 
 
 pilas.iniciar()
-a = Globo("Hola mundo")
+a = Globo("Hola mundo, este.")
 pilas.ejecutar()
