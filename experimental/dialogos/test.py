f1143b6cc8dd3ea8fd8f71902497a00d931dd9b5
hugoruscitti <devnull@localhost>
2011-01-23 17:17:50 -0300

    Creando un ejemplo con dialogos que tienen preguntas.

--- experimental/dialogos/test.py
+++ experimental/dialogos/test.py
@@ -2,26 +2,32 @@
 import pilas
 
 class Globo(pilas.actores.Actor):
-    
+
     def __init__(self, texto, x=0, y=0):
         pilas.actores.Actor.__init__(self, x=x, y=y)
         ancho, alto = self._crear_lienzo(texto, pilas) 
         imagen = pilas.imagenes.cargar_imagen_cairo("globo.png")
 
         self._pintar_globo(x, y, ancho, alto, imagen)
-        
         self.lienzo.definir_color(pilas.colores.negro)
-        self.lienzo.escribir(texto, 12, 25, tamano=14)
+        self._escribir_texto(texto)
         self.lienzo.asignar(self)
         self.centro = ("derecha", "abajo")
 
+    def _escribir_texto(self, texto):
+        return self.lienzo.escribir(texto, 12, 25, tamano=14)
+    
     def _crear_lienzo(self, texto, pilas):
         self.lienzo = pilas.lienzo.Lienzo(10, 10)
-        ancho, alto = self.lienzo.obtener_area_de_texto(texto, tamano=14)
+        ancho, alto = self._obtener_area_para_el_texto(texto)
         ancho = int((ancho + 12) - (ancho % 12))
         alto = int((alto + 12) - alto % 12)
         self.lienzo = pilas.lienzo.Lienzo(ancho + 36, alto + 24 + 35)
         return ancho, alto
+
+    def _obtener_area_para_el_texto(self, texto):
+        return self.lienzo.obtener_area_de_texto(texto, tamano=14)
+
 
     def _pintar_globo(self, x, y, ancho, alto, imagen):
         # esquina sup-izq
@@ -54,6 +60,31 @@ class Globo(pilas.actores.Actor):
         self.lienzo.pintar_parte_de_imagen(imagen, 67, 35, 33, 25, int(ancho) - 12, 0 + int(alto) + 12 + 12)
 
 
+class GloboElegir(Globo):
+    
+    def __init__(self, texto, opciones, funcion_a_invocar, x=0, y=0):
+        self.opciones = opciones
+        self.funcion_a_invocar = funcion_a_invocar
+        Globo.__init__(self, texto, x, y)
+
+    def _obtener_area_para_el_texto(self, texto):
+        ancho, alto = self.lienzo.obtener_area_de_texto(texto, tamano=14)
+
+        # Aumenta el tamano para las opciones
+        for opcion in self.opciones:
+            ancho_opcion, alto_opcion = self.lienzo.obtener_area_de_texto(opcion, tamano=14)
+            ancho = max(ancho, ancho_opcion)
+            alto += alto_opcion + 10
+            print alto_opcion
+
+        return ancho, alto
+        
+    def _escribir_texto(self, texto):
+        self.lienzo.escribir(texto, 12, 25, tamano=14)
+        
+        for (indice, respuesta) in enumerate(self.opciones):
+            self.lienzo.escribir(respuesta, 12, 25 + 30 + indice * 20, tamano=14)    
+    
 class Dialogo:
     
     def __init__(self):
@@ -62,6 +93,9 @@ class Dialogo:
     
     def decir(self, actor, texto):
         self.dialogo.append((actor, texto))
+    
+    def elegir(self, actor, texto, opciones, funcion_a_invocar):
+        self.dialogo.append((actor, texto, opciones, funcion_a_invocar))
         
     def ejecutar(self, funcion):
         self.dialogo.append(funcion)
@@ -82,8 +116,15 @@ class Dialogo:
 
     def _mostrar_o_ejecutar_siguiente(self, siguiente):
         if isinstance(siguiente, tuple):
-            actor, texto = siguiente
-            self.dialogo_actual = Globo(texto, x=actor.x + 30, y=actor.y + 30)
+            # Es un mensaje de dialogo simple
+            if len(siguiente) == 2:
+                actor, texto = siguiente
+                self.dialogo_actual = Globo(texto, x=actor.x + 30, y=actor.y + 30)
+            else:
+                # Es un mensaje con seleccion.
+                actor, texto, opciones, funcion_a_invocar = siguiente
+                print texto, opciones
+                self.dialogo_actual = GloboElegir(texto, opciones, funcion_a_invocar, x=actor.x + 30, y=actor.y + 30)
         else:
             siguiente()
 
@@ -106,6 +147,7 @@ mono = pilas.actores.Mono(x=-100)
 mono_chiquito = pilas.actores.Mono(x=200)
 mono_chiquito.escala = 0.75
 
+"""
 d = Dialogo()
 d.decir(mono, "Hola, como estas?")
 d.decir(mono_chiquito, "Bien, ¿y vos?...")
@@ -117,6 +159,16 @@ def hacer_que_el_mono_salte():
 
 d.ejecutar(hacer_que_el_mono_salte)
 d.decir(mono_chiquito, "wow...")
+"""
+
+d = Dialogo()
+d.decir(mono, "¿Cual es tu color favorito?")
+
+def cuando_responde_color_favorito(respuesta):
+    d.decir(mono, "Ya esta, ahora tenemos fondo '%s" %(respuesta))
+    d.decir(mono_chiquito, "fua...")
+    
+d.elegir(mono_chiquito, "Mi color favorito es el...", ["rojo", "verde", "azul"], cuando_responde_color_favorito)
 
 
 d.iniciar()
