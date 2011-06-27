0d32ec9b887c3908cfc69c206bb6a5de0f314a3f
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-06-27 16:03:43 -0300

    reparando el actor de interfaz para ingresar texto

--- pilas/interfaz/ingreso_de_texto.py
+++ pilas/interfaz/ingreso_de_texto.py
@@ -10,45 +10,42 @@ import pilas
 
 class IngresoDeTexto(pilas.actores.Actor):
     
-    def __init__(self, x=0, y=0):
+    def __init__(self, texto_inicial="", x=0, y=0):
         pilas.actores.Actor.__init__(self, x=x, y=y)
-        self.texto = ""
-        self.cursor = "|"
+        self.texto = texto_inicial
+        self.cursor = ""
         self._cargar_lienzo()
-        self._cargar_imagenes(pilas)
+        self.imagen_caja = pilas.imagenes.cargar("interfaz/caja.png")
+        self.centro = ("centro", "centro")
         self._actualizar_imagen()
+
 
         pilas.eventos.pulsa_tecla.conectar(self.cuando_pulsa_una_tecla)
         pilas.mundo.agregar_tarea_siempre(0.25, self._actualizar_cursor)
         
     def _actualizar_cursor(self):
         if self.cursor == "":
-            self.cursor = "|"
+            self.cursor = "_"
         else:
             self.cursor = ""
             
         self._actualizar_imagen()
         return True
         
-    def _cargar_imagenes(self, pilas):
-        self.imagen_caja = pilas.imagenes.cargar_imagen_cairo("interfaz/caja.png")
         
     def cuando_pulsa_una_tecla(self, evento):
-        if evento.codigo == '\x08':
+        if evento.codigo == '\x08' or evento.texto == '\x08':
             # Indica que se quiere borrar un caracter
             self.texto = self.texto[:-1]
         else:
             if len(self.texto) < 23:
-                self.texto = self.texto + evento.codigo
+                self.texto = self.texto + evento.texto
         
         self._actualizar_imagen()
         
     def _cargar_lienzo(self):
-        self.lienzo = pilas.imagenes.cargar_lienzo(333, 30)
+        self.imagen = pilas.imagenes.cargar_superficie(400, 200)
         
     def _actualizar_imagen(self):
-        self.lienzo.pintar_imagen(self.imagen_caja)
-        self.lienzo.definir_color(pilas.colores.negro)
-        self.lienzo.escribir(self.texto + self.cursor, 35, 20, tamano=14, fuente='sans')
-        self.lienzo.asignar(self)
-        self.centro = ("centro", "centro")
+        self.imagen.pintar_imagen(self.imagen_caja)
+        self.imagen.texto(self.texto + self.cursor, 35, 20)
