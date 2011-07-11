75e5af464b4df84e48dd4af39d3dc3199cf9b46c
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-07-10 23:22:47 -0300

    cambiando el area de la lista de seleccion y permitiendo no colocar una funcion de respuesta.

--- pilas/interfaz/lista_seleccion.py
+++ pilas/interfaz/lista_seleccion.py
@@ -11,13 +11,13 @@ from pilas.actores import Actor
 
 class ListaSeleccion(Actor):
 
-    def __init__(self, opciones, funcion_a_ejecutar, x=0, y=0):
+    def __init__(self, opciones, funcion_a_ejecutar=None, x=0, y=0):
         Actor.__init__(self, x=x, y=y)
         self.opciones = opciones
         self.funcion_a_ejecutar = funcion_a_ejecutar
         
         ancho, alto = pilas.mundo.motor.obtener_area_de_texto("\n".join(opciones))
-        self.imagen = pilas.imagenes.cargar_superficie(int(ancho + 35), int(alto))
+        self.imagen = pilas.imagenes.cargar_superficie(int(ancho + 35), int(alto + 5))
 
         self._pintar_opciones()
         
@@ -42,10 +42,12 @@ class ListaSeleccion(Actor):
     def cuando_hace_click_con_el_mouse(self, evento):
         if self.colisiona_con_un_punto(evento.x, evento.y):
             opcion = self._detectar_opcion_bajo_el_mouse(evento)
-            self.funcion_a_ejecutar(self.opciones[opcion])
+            if self.funcion_a_ejecutar:
+                self.funcion_a_ejecutar(self.opciones[opcion])
+            else:
+                print "Cuidado, no has definido funcion a ejecutar en la lista de seleccion."
 
     def _detectar_opcion_bajo_el_mouse(self, evento):
         opcion = int((self.arriba - evento.y ) / 20)
         if opcion in range(0, len(self.opciones)):
             return opcion
-
