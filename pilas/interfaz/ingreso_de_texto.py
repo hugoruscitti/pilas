95f5226accc1b851089cb5aa2b5407ed8c5e0587
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-07-14 18:47:42 -0300

    haciendo que el componente ingreso de texto permita cambiar el tamano del componente, ponerle limites de caracteres y funciones de validacion

--- pilas/interfaz/ingreso_de_texto.py
+++ pilas/interfaz/ingreso_de_texto.py
@@ -7,10 +7,11 @@
 # Website - http://www.pilas-engine.com.ar
 
 import pilas
+import re
 
 class IngresoDeTexto(pilas.actores.Actor):
     
-    def __init__(self, texto_inicial="", x=0, y=0, ancho=300):
+    def __init__(self, texto_inicial="", x=0, y=0, ancho=300, limite_de_caracteres=20):
         pilas.actores.Actor.__init__(self, x=x, y=y)
         self.texto = texto_inicial
         self.cursor = ""
@@ -18,7 +19,8 @@ class IngresoDeTexto(pilas.actores.Actor):
         self.imagen_caja = pilas.imagenes.cargar("interfaz/caja.png")
         self.centro = ("centro", "centro")
         self._actualizar_imagen()
-
+        self.limite_de_caracteres = limite_de_caracteres
+        self.cualquier_caracter()
 
         pilas.eventos.pulsa_tecla.conectar(self.cuando_pulsa_una_tecla)
         pilas.mundo.agregar_tarea_siempre(0.40, self._actualizar_cursor)
@@ -31,15 +33,30 @@ class IngresoDeTexto(pilas.actores.Actor):
             
         self._actualizar_imagen()
         return True
-        
+
+    def cualquier_caracter(self):
+        self.caracteres_permitidos = re.compile(".*")
+
+    def solo_numeros(self):
+        self.caracteres_permitidos = re.compile("\d+")
+
+    def solo_letras(self):
+        self.caracteres_permitidos = re.compile("[a-z]+")
         
     def cuando_pulsa_una_tecla(self, evento):
         if evento.codigo == '\x08' or evento.texto == '\x08':
             # Indica que se quiere borrar un caracter
             self.texto = self.texto[:-1]
         else:
-            if len(self.texto) < 23:
-                self.texto = self.texto + evento.texto
+            if len(self.texto) < self.limite_de_caracteres:
+                nuevo_texto = self.texto + evento.texto
+
+                if (self.caracteres_permitidos.match(evento.texto)):
+                    self.texto = self.texto + evento.texto
+                else:
+                    print "Rechazando el ingreso del caracter:", evento.texto
+            else:
+                print "Rechazando caracter por llegar al limite."
         
         self._actualizar_imagen()
         
@@ -53,5 +70,5 @@ class IngresoDeTexto(pilas.actores.Actor):
 
         for x in range(40, self.imagen.ancho() - 40):
             self.imagen.pintar_parte_de_imagen(self.imagen_caja, ancho - 40, 0, 40, alto, x, 0)
-        #self.imagen.pintar_parte_de_imagen(self.imagen_caja, ancho - 40, 0, 40, alto, 40, 0)
+
         self.imagen.texto(self.texto + self.cursor, 35, 20)
