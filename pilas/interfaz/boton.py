481639c3c81082c392a6b80c361f7dac065002ca
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-07-26 09:27:28 -0300

    creando el actor boton con texto como etiqueta.

--- pilas/interfaz/boton.py
+++ pilas/interfaz/boton.py
@@ -14,24 +14,40 @@ class Boton(pilas.actores.Actor):
         pilas.actores.Actor.__init__(self, x=x, y=y)
         self.texto = texto
         self._crear_imagenes_de_botones()
+        self.centro = ("centro", "centro")
+        pilas.eventos.mueve_mouse.conectar(self.cuando_mueve_el_mouse)
 
         if icono:
             self.icono = pilas.imagenes.cargar(icono)
         else:
             self.icono = None
 
-        self.imagen_normal = pilas.imagenes.cargar("interfaz/caja.png")
-        self.centro = ("centro", "centro")
         
     def _crear_imagenes_de_botones(self):
+        "Genera las 3 imagenes de los botones."
         ancho, alto = pilas.utils.obtener_area_de_texto(self.texto)
-        self.imagen = pilas.imagenes.cargar_superficie(20 + ancho, 30)
         tema = pilas.imagenes.cargar("boton/tema.png")
 
-        self.imagen.pintar_parte_de_imagen(tema, 0, 0, 5, 25, 0, 0)
+        self.imagen_normal = self._crear_imagen(tema, self.texto, ancho, 0)
+        self.imagen_sobre = self._crear_imagen(tema, self.texto, ancho, 103)
+        self.imagen_click = self._crear_imagen(tema, self.texto, ancho, 205)
+
+        self.imagen = self.imagen_normal
+
+    def cuando_mueve_el_mouse(self, evento):
+        if self.colisiona_con_un_punto(evento.x, evento.y):
+            self.imagen = self.imagen_sobre
+        else:
+            self.imagen = self.imagen_normal
+        
+    def _crear_imagen(self, tema, texto, ancho, dx):
+        "Genera una imagen de superficie de boton."
+        imagen = pilas.imagenes.cargar_superficie(20 + ancho, 30)
+        imagen.pintar_parte_de_imagen(tema, dx, 0, 5, 25, 0, 0)
 
         for x in range(1, ancho + 20, 5):
-            self.imagen.pintar_parte_de_imagen(tema, 5, 0, 5, 25, x, 0)
+            imagen.pintar_parte_de_imagen(tema, dx + 5, 0, 5, 25, x, 0)
 
-        self.imagen.pintar_parte_de_imagen(tema, 75, 0, 5, 25, ancho + 15, 0)
-        self.imagen.texto(self.texto, 10, 17)
+        imagen.pintar_parte_de_imagen(tema, dx + 75, 0, 5, 25, ancho + 15, 0)
+        imagen.texto(texto, 10, 17)
+        return imagen
