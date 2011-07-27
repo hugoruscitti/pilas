1854516964fd9b2097fc9c8cf97f49c6ac790c65
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-07-27 15:11:07 -0300

    Creando script para actualizar los paquetes en pypi

---
+++ utils/actualizar_pypi.py
@@ -0,0 +1,10 @@
+import os
+
+def renombrar_instalador_para_windows():
+    lista = os.listdir('dist/')
+    instalador_windows = [item for item in lista if item.endswith(".exe")]
+    nombre = "dist/" + instalador_windows[0]
+    os.rename(nombre, nombre.replace("linux-i686", "win32"))
+
+os.system("python setup.py sdist")
+os.system("python setup.py bdist_wininst")
