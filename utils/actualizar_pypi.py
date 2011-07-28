16e3dcf6fced3590fd616bdee3d8b486d8688a1c
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-07-28 20:17:23 -0300

    actualizando el script para subir pilas a pypi

--- utils/actualizar_pypi.py
+++ utils/actualizar_pypi.py
@@ -6,5 +6,9 @@ def renombrar_instalador_para_windows():
     nombre = "dist/" + instalador_windows[0]
     os.rename(nombre, nombre.replace("linux-i686", "win32"))
 
-os.system("python setup.py sdist")
+
+os.system("rm dist/*")
+os.system("python setup.py sdist upload")
 os.system("python setup.py bdist_wininst")
+renombrar_instalador_para_windows()
+os.system("python setup.py bdist_wininst upload")
