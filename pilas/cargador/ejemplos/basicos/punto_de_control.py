5c4f43b16b830244491814ae8305808c9a775c5b
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-08-02 23:17:37 -0300

    actualizando imagenes de los juegos de ejemplos que tiene el cargador de pilas.

--- pilas/cargador/ejemplos/basicos/punto_de_control.py
+++ pilas/cargador/ejemplos/basicos/punto_de_control.py
@@ -11,7 +11,7 @@ pingu1 = pilas.actores.Pingu(x=-100)
 pingu2 = pilas.actores.Pingu(x=+100)
 
 # El pinguino de la derecha tendra el punto de control en los pies.
-pingu2.centro = ('centro', 'abajo')
+pingu2.centro = ('centro', 'arriba')
 
 pilas.avisar("Dos actores con distintos centros (puntos de control), pulsa F12.")
 pilas.ejecutar()
