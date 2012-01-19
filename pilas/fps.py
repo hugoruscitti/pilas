4db8c5c8a85ff887ef8cf23a1d44b21cfb6e543e
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-01-19 13:08:36 -0300

    limpiando el archivo fps.py

--- pilas/fps.py
+++ pilas/fps.py
@@ -1,58 +1,5 @@
 # -*- encoding: utf-8 -*-
-import time
 from PyQt4 import QtCore
-
-
-class _FPS(object):
-
-    def __init__(self, fps, usar_modo_economico):
-        self.antes = self.ahora = pygame.time.get_ticks()
-        self.frecuencia = 1000.0 / fps
-        self.t_fps = self.ahora
-        self.rendimiento = 0
-        self.cuadros_por_segundo = "??"
-        self.usar_modo_economico = usar_modo_economico
-        self.inicial = time.time()
-
-    def actualizar(self):
-        retorno = 0
-        self.ahora = pygame.time.get_ticks()
-        # a la velocidad de los segundos, mill por segundo
-        #print self.ahora, time.time() - self.inicial
-
-        dt = self.ahora - self.antes 
-
-        while dt >= self.frecuencia:
-            self.antes += self.frecuencia
-            dt = self.ahora - self.antes 
-            retorno += 1
-
-            if self.ahora - self.t_fps > 1000.0:
-                #print self.cuadros_por_segundo
-                self.cuadros_por_segundo = str(self.rendimiento)
-                self.t_fps += 1000.0
-                self.rendimiento = 0
-        else:
-            if self.usar_modo_economico:
-                #time.sleep(1)
-                pygame.time.wait(int(self.frecuencia - dt))
-                #pygame.time.wait(0)
-            else:
-                pass
-                #pygame.time.delay(int(self.frecuencia - dt))
-
-            returno = 1
-
-
-        self.rendimiento += 1
-        return retorno
-
-
-    def obtener_cuadros_por_segundo(self):
-        return self.cuadros_por_segundo
-
-
-# parece que hay que usar time.time en unix y time.clock en windows
 
 class FPS(object):
 
@@ -90,45 +37,6 @@ class FPS(object):
             self.ultimo_reporte_fps += 1000.0
             self.cuadros_por_segundo = str(self.cuadros)
             self.cuadros = 0
-
-
-        #sleepTime = self.frecuencia - (newTime - self.delay)
-        #if sleepTime > 0:
-        #    time.sleep(sleepTime)
-        #self.delay = newTime
-        #return 1
-
-        """
-
-
-
-
-        while dt >= self.frecuencia:
-            self.antes += self.frecuencia
-            dt = self.ahora - self.antes 
-            retorno += 1
-
-            if self.ahora - self.t_fps > 1000.0:
-                #print self.cuadros_por_segundo
-                self.cuadros_por_segundo = str(self.rendimiento)
-                self.t_fps += 1000.0
-                self.rendimiento = 0
-        else:
-            if self.usar_modo_economico:
-                #time.sleep(1)
-                pygame.time.wait(int(self.frecuencia - dt))
-                #pygame.time.wait(0)
-            else:
-                pass
-                #pygame.time.delay(int(self.frecuencia - dt))
-
-            returno = 1
-
-
-        self.rendimiento += 1
-        return retorno
-        """
-
 
     def obtener_cuadros_por_segundo(self):
         return self.cuadros_por_segundo
