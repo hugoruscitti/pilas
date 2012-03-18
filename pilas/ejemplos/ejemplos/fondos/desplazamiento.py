528c4f6f06f37e78f932419bc79a151377945f2b
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-03-17 20:24:42 -0600

    creando ejemplo sobre fondo con desplazamiento.

---
+++ pilas/ejemplos/ejemplos/fondos/desplazamiento.py
@@ -0,0 +1,32 @@
+import pilas
+pilas.iniciar(usar_motor='qtgl')
+
+cielo = pilas.actores.Actor("cielo.png")
+montes = pilas.actores.Actor("montes.png")
+arboles = pilas.actores.Actor("arboles.png")
+pasto = pilas.actores.Actor("pasto.png", y=-179)
+
+cielo.z = 10
+montes.z = 5
+arboles.z = 0
+pasto.z = -10
+
+fondo = pilas.fondos.Desplazamiento()
+
+fondo.agregar(cielo, 0)
+fondo.agregar(montes, 0.5)
+fondo.agregar(arboles, 0.9)
+fondo.agregar(pasto, 2)
+
+"""
+# Mueve la posicion con un temporizador.
+def mover():
+    fondo.posicion += 3
+    return True
+                    
+pilas.mundo.tareas.siempre(1/15.0, mover)
+"""
+
+pilas.mundo.camara.x = [200], 10
+
+pilas.ejecutar()
