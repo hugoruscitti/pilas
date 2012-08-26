9c9532a6252fd4381048f0ddff926589a3a9f9ce
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-08-26 12:36:00 -0300

    Haciendo que funcione lanzar el cargador de ejemplos y las rutas dentro del HTML del asistente.

--- pilas/asistente.py
+++ pilas/asistente.py
@@ -10,28 +10,41 @@ class Ventana(Ui_Main):
 
     def setupUi(self, Main):
         Ui_Main.setupUi(self, Main)
-        base_path = utils.obtener_ruta_al_recurso('asistente')
-        contenido = self._obtener_html()
+
         self.webView.page().setLinkDelegationPolicy(QtWebKit.QWebPage.DelegateAllLinks)
-
         self.webView.connect(self.webView, QtCore.SIGNAL("linkClicked(const QUrl&)"), self.cuando_pulsa_link)
+        self._cargar_pagina_principal()
+        self.main = Main
 
-        self.webView.setHtml(contenido, QtCore.QUrl(base_path))
 
-    def _obtener_html(self):
+    def _cargar_pagina_principal(self):
         file_path = utils.obtener_ruta_al_recurso('asistente/index.html')
+
+        contenido = self._obtener_html(file_path)
+        base_dir =  QtCore.QUrl.fromLocalFile(file_path)
+
+        self.webView.setHtml(contenido, base_dir)
+
+    def _obtener_html(self, file_path):
         archivo = open(file_path, "rt")
         contenido = archivo.read()
         archivo.close()
         return contenido.replace("{{VERSION}}", pilas.version())
 
     def cuando_pulsa_link(self, url):
-        seccion = str(url.path())
+        seccion = str(url.path()).split('/')[-1]
 
         if seccion == "interprete":
             pilas.abrir_interprete()
         elif seccion == "ejemplos":
-            pilas.abrir_cargador()
+            from ejemplos import cargador
+            ventana = cargador.VentanaPrincipal()
+
+            dialogo = QtGui.QDialog(self.main)
+            ventana.setParent(dialogo)
+            dialogo.show()
+            dialogo.raise_()
+
         elif seccion == "manual":
             print "El manual"
         elif seccion == "web":
