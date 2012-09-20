87bea0610b9307e3394e58c9069ca060998d15fc
Quique Porta <quiqueporta@gmail.com>
2012-09-20 09:55:11 +0200

    Modificado el cambio de escenas ya que siempre el gestor de escenas es accesible desde Mundo.

--- pilas/ejemplos/ejemplos/escenas/escenas_apiladas.py
+++ pilas/ejemplos/ejemplos/escenas/escenas_apiladas.py
@@ -20,10 +20,10 @@ class EscenaDeMenu(EscenaNormal):
         self.menu = pilas.actores.Menu(opciones)
         
     def comenzar(self):
-        self.gestor_escenas.cambiar_escena(EscenaDeJuego())
+        pilas.mundo.gestor_escenas.cambiar_escena(EscenaDeJuego())
         
     def opciones(self):
-        self.gestor_escenas.almacenar_escena(EscenaDeOpciones())
+        pilas.mundo.gestor_escenas.almacenar_escena(EscenaDeOpciones())
 
     def salir(self):
         import sys
@@ -46,11 +46,11 @@ Si vas a opciones y regresas el pinguino\n seguira en la misma posicion donde \n
         self.pulsa_tecla.conectar(self.cuando_pulsa_tecla)
         
     def ir_a_menu(self, evento):
-        self.gestor_escenas.cambiar_escena(EscenaDeMenu())
+        pilas.mundo.gestor_escenas.cambiar_escena(EscenaDeMenu())
         
     def cuando_pulsa_tecla(self, evento):
         if evento.texto == u'o':
-            self.gestor_escenas.almacenar_escena(EscenaDeOpciones())
+            pilas.mundo.gestor_escenas.almacenar_escena(EscenaDeOpciones())
     
 class EscenaDeOpciones(EscenaNormal):
 
@@ -73,7 +73,7 @@ class EscenaDeOpciones(EscenaNormal):
         pilas.avisar("Esto no hace nada.")
 
     def ir_a_escena_anterior(self, evento):
-        self.gestor_escenas.recuperar_escena()
+        pilas.mundo.gestor_escenas.recuperar_escena()
 
 # Carga la nueva escena
 pilas.mundo.gestor_escenas.cambiar_escena(EscenaDeMenu())
