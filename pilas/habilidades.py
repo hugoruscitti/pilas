539df3a0916a5547592981e16e092a44401f07a8
Quique Porta <quiqueporta@gmail.com>
2012-10-13 14:16:49 +0200

    Añadida a la habilidad de SeMantieneEnPantalla un parametro para que el actor no pueda salir de la ventana de juego. Por defecto si sale, aparece por el lado opuesto por donde salió.

--- pilas/habilidades.py
+++ pilas/habilidades.py
@@ -194,27 +194,45 @@ class PuedeExplotar(Habilidad):
 
 
 class SeMantieneEnPantalla(Habilidad):
-    """Se asegura de que el actor regrese a la pantalla si sale.
+    """Se asegura de que el actor regrese a la pantalla si sale o que no
+    salga en nigún momento de la pantalla.
 
     Si el actor sale por la derecha de la pantalla, entonces regresa
-    por la izquiera. Si sale por arriba regresa por abajo y asi..."""
-    def __init__(self, receptor):
+    por la izquiera. Si sale por arriba regresa por abajo y asi...
+    
+    param: permitir_salida: Valor booleano que establece si el actor
+    puede salir por los lados de la ventana y regresar por el lado opuesto.
+    Si se establece a False, el actor no puede salir de la ventana en ningún
+    momento.
+    """
+    def __init__(self, receptor, permitir_salida=True):
         Habilidad.__init__(self, receptor)
         self.ancho, self.alto = pilas.mundo.motor.obtener_area()
+        self.permitir_salida = permitir_salida
 
     def actualizar(self):
-        # Se asegura de regresar por izquierda y derecha.
-        if self.receptor.derecha < -(self.ancho/2):
-            self.receptor.izquierda = (self.ancho/2)
-        elif self.receptor.izquierda > (self.ancho/2):
-            self.receptor.derecha = -(self.ancho/2)
+        if self.permitir_salida :
+            # Se asegura de regresar por izquierda y derecha.
+            if self.receptor.derecha < -(self.ancho/2):
+                self.receptor.izquierda = (self.ancho/2)
+            elif self.receptor.izquierda > (self.ancho/2):
+                self.receptor.derecha = -(self.ancho/2)
+    
+            # Se asegura de regresar por arriba y abajo.
+            if self.receptor.abajo > (self.alto/2):
+                self.receptor.arriba = -(self.alto/2)
+            elif self.receptor.arriba < -(self.alto/2):
+                self.receptor.abajo = (self.alto/2)
+        else:
+            if self.receptor.izquierda <= -(self.ancho/2):
+                self.receptor.izquierda = -(self.ancho/2)
+            elif self.receptor.derecha >=  (self.ancho/2):
+                self.receptor.derecha = self.ancho/2
 
-        # Se asegura de regresar por arriba y abajo.
-        if self.receptor.abajo > (self.alto/2):
-            self.receptor.arriba = -(self.alto/2)
-        elif self.receptor.arriba < -(self.alto/2):
-            self.receptor.abajo = (self.alto/2)
-
+            if self.receptor.arriba > (self.alto/2):
+                self.receptor.arriba = (self.alto/2)
+            elif self.receptor.abajo < -(self.alto/2):
+                self.receptor.abajo = -(self.alto/2)
 
 class PisaPlataformas(Habilidad):
 
