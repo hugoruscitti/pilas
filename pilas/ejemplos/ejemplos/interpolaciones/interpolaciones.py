03427a202b4a59b3532d10760d9ad5ba8c4a4906
Quique Porta <quiqueporta@gmail.com>
2012-10-14 19:47:47 +0200

    Ref #32 Actualizado el ejemplo de interpolaciones. Añadido un boton para detenerla.

--- pilas/ejemplos/ejemplos/interpolaciones/interpolaciones.py
+++ pilas/ejemplos/ejemplos/interpolaciones/interpolaciones.py
@@ -10,6 +10,9 @@ def cuando_selecciona(opcion_seleccionada):
     actor.y = 200
     actor.y = pilas.interpolar(-200,2,tipo=opcion_seleccionada)
 
+def detener_interpolacion():
+    pilas.utils.deneter_interpolacion(actor, 'y')        
+
 opciones = pilas.interfaz.ListaSeleccion(['lineal',
                                           'aceleracion_gradual',
                                           'desaceleracion_gradual',
@@ -17,6 +20,11 @@ opciones = pilas.interfaz.ListaSeleccion(['lineal',
                                           'rebote_final'], cuando_selecciona)
 opciones.x = -200
 opciones.y = 150
+
+boton = pilas.interfaz.Boton("Detener Interpolacion")
+boton.conectar(detener_interpolacion)
+
+boton.x = -200
 
 actor = pilas.actores.Banana()
 actor.y = 200
