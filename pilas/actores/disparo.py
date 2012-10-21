046d46af8db3f80ca34df5b938cb0a35009e2344
Quique Porta <quiqueporta@gmail.com>
2012-10-21 19:23:16 +0200

    Añadido el radio de colisión a la clase Disparo y establecida una imagen por defecto.

--- pilas/actores/disparo.py
+++ pilas/actores/disparo.py
@@ -13,16 +13,17 @@ import math
 class Disparo(Animacion):
     "Representa un disparo que avanza."
 
-    def __init__(self, x=0, y=0, rotacion=0, velocidad=2):
+    def __init__(self, x=0, y=0, rotacion=0, velocidad=2,aceleracion=1,
+                 radio_de_colision=10):
         self.velocidad = velocidad
-        grilla = pilas.imagenes.cargar_grilla("disparo.png", 2)
+        grilla = pilas.imagenes.cargar_grilla("sin_imagen.png", 1)
         Animacion.__init__(self, grilla, ciclica=True, x=x, y=y)
-        self.radio_de_colision = 10
+        self.radio_de_colision = radio_de_colision
         self.rotacion = rotacion
 
     def actualizar(self):
         Animacion.actualizar(self)
-        #self.avanzar()
+        self.avanzar()
 
     def avanzar(self):
         "Hace avanzar la nave en direccion a su angulo."
@@ -31,3 +32,6 @@ class Disparo(Animacion):
         dy = math.sin(rotacion_en_radianes) * self.velocidad
         self.x += dx
         self.y += dy
+
+   
+        
