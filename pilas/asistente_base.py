8a0183a1057d0089265d55d109dedaf2841605b5
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-11-02 01:17:56 -0300

    Agregando opción para salir del asistente desde un menú fix #101

--- pilas/asistente_base.py
+++ pilas/asistente_base.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'asistente.ui'
 #
-# Created: Sat Oct  6 20:19:10 2012
+# Created: Fri Nov  2 01:12:59 2012
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -19,7 +19,7 @@ class Ui_AsistenteWindow(object):
         AsistenteWindow.setObjectName(_fromUtf8("AsistenteWindow"))
         AsistenteWindow.resize(507, 342)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8("pilas.ico")), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("pilas.ico")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         AsistenteWindow.setWindowIcon(icon)
         self.centralwidget = QtGui.QWidget(AsistenteWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
@@ -34,16 +34,25 @@ class Ui_AsistenteWindow(object):
         self.menubar = QtGui.QMenuBar(AsistenteWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 507, 22))
         self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menu_Archivo = QtGui.QMenu(self.menubar)
+        self.menu_Archivo.setObjectName(_fromUtf8("menu_Archivo"))
         AsistenteWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(AsistenteWindow)
         self.statusbar.setObjectName(_fromUtf8("statusbar"))
         AsistenteWindow.setStatusBar(self.statusbar)
+        self.salir_action = QtGui.QAction(AsistenteWindow)
+        self.salir_action.setObjectName(_fromUtf8("salir_action"))
+        self.menu_Archivo.addSeparator()
+        self.menu_Archivo.addAction(self.salir_action)
+        self.menubar.addAction(self.menu_Archivo.menuAction())
 
         self.retranslateUi(AsistenteWindow)
         QtCore.QMetaObject.connectSlotsByName(AsistenteWindow)
 
     def retranslateUi(self, AsistenteWindow):
         AsistenteWindow.setWindowTitle(QtGui.QApplication.translate("AsistenteWindow", "pilas-engine", None, QtGui.QApplication.UnicodeUTF8))
+        self.menu_Archivo.setTitle(QtGui.QApplication.translate("AsistenteWindow", "&Archivo", None, QtGui.QApplication.UnicodeUTF8))
+        self.salir_action.setText(QtGui.QApplication.translate("AsistenteWindow", "Salir", None, QtGui.QApplication.UnicodeUTF8))
 
 from PyQt4 import QtWebKit
 
