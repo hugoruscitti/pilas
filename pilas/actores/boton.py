c14e900348e6cb41b6243fdc28d78aeffddd669e
Quique Porta <quiqueporta@gmail.com>
2012-11-13 22:26:56 +0100

    Arreglada la API del Boton. Ref #117

--- pilas/actores/boton.py
+++ pilas/actores/boton.py
@@ -11,25 +11,39 @@
 from pilas.actores import Actor
 import pilas
 
+
 class Boton(Actor):
     """Representa un boton que reacciona al ser presionado."""
-	
-    def __init__(self, x=0, y=0, 
-                ruta_normal = 'boton/boton_normal.png',
-                ruta_press = 'boton/boton_press.png',
-                ruta_over = 'boton/boton_over.png',
+
+    def __init__(self, x=0, y=0,
+                ruta_normal='boton/boton_normal.png',
+                ruta_press='boton/boton_press.png',
+                ruta_over='boton/boton_over.png',
                 ):
+        """ Constructor del Boton.
+
+        :param x: Posición horizontal del Actor.
+        :type x: int
+        :param y: Posición vertical del Actor.
+        :type y: int
+        :param ruta_normal: Ruta de la imagen del boton en estado normal.
+        :type ruta_normal: string
+        :param ruta_press: Ruta de la imagen del boton cuando se presiona.
+        :type ruta_press: string
+        :param ruta_over: Ruta de la imagen del boton cuando el ratón pasa por encima.
+        :type ruta_over: string
+        """
 
         self.ruta_normal = ruta_normal
         self.ruta_press = ruta_press
         self.ruta_over = ruta_over
-        
+
         self.funciones_normal = []
         self.funciones_press = []
         self.funciones_over = []
-        
+
         self.estado = True
-        
+
         Actor.__init__(self, ruta_normal, x=x, y=y)
         self._cargar_imagenes(self.ruta_normal, self.ruta_press, self.ruta_over)
 
@@ -45,41 +59,91 @@ class Boton(Actor):
         self.imagen_over = pilas.imagenes.cargar(ruta_over)
         self.imagen_normal = pilas.imagenes.cargar(ruta_normal)
         self.imagen_press = pilas.imagenes.cargar(ruta_press)
-    
 
-    #funciones que conectan evento(press, over, normal) a funciones 
-    def conectar_normal(self, funcion, arg = "null"):
+    #funciones que conectan evento(press, over, normal) a funciones
+    def conectar_normal(self, funcion, arg="null"):
+        """ Permite conectar un metodo para que sea ejecutado cuando el botón
+        pase al estado normal.
+
+        >>> def cuando_deja_de_pulsar():
+        >>>     b.pintar_normal()
+        >>>
+        >>> mi_boton.conectar_normal(cuando_deja_de_pulsar)
+
+        :param funcion: Método a llamar cuando el botón pase a estado Normal.
+        :param arg: Argumentos a pasar a la funcion.
+        """
         t = (funcion, arg)
         self.funciones_normal.append(t)
-    
-    def conectar_presionado(self, funcion, arg = "null"):
+
+    def conectar_presionado(self, funcion, arg="null"):
+        """ Permite conectar un metodo para que sea ejecutado cuando el botón
+        se presiona.
+
+        >>> def cuando_pulsan_el_boton():
+        >>>     b.pintar_presionado()
+        >>>
+        >>> mi_boton.conectar_presionado(cuando_pulsan_el_boton)
+
+        :param funcion: Método a llamar cuando el botón pase a estado Normal.
+        :param arg: Argumentos a pasar a la funcion.
+        """
         t = (funcion, arg)
         self.funciones_press.append(t)
-    
-    def conectar_sobre(self, funcion, arg = "null"):
+
+    def conectar_sobre(self, funcion, arg="null"):
+        """ Permite conectar un metodo para que sea ejecutado cuando el ratón
+        pasa por encima del botón.
+
+        >>> def cuando_pasa_sobre_el_boton():
+        >>>     b.pintar_sobre()
+        >>>
+        >>> mi_boton.conectar_sobre(cuando_pasa_sobre_el_boton)
+
+        :param funcion: Método a llamar cuando el botón pase a estado Normal.
+        :param arg: Argumentos a pasar a la funcion.
+        """
         t = (funcion, arg)
-        self.funciones_over.append(t)    
+        self.funciones_over.append(t)
 
     def desconectar_normal_todo(self):
+        """ Elimina todas las funciones asociadas al estado normal del botón. """
         self.funciones_normal = []
-    
+
     def desconectar_presionado_todo(self):
+        """ Elimina todas las funciones asociadas al estado presionado del botón. """
         self.funciones_press = []
-    
+
     def desconectar_sobre_todo(self):
+        """ Elimina todas las funciones asociadas al estado sobre del botón. """
         self.funciones_over = []
-        
-    def desconectar_normal(self, funcion, arg = "null"):
+
+    def desconectar_normal(self, funcion, arg="null"):
+        """ Elimina el método indicado asociado al estado normal del botón.
+
+        :param funcion: Método al que se llama cuando el botón pasa estado Normal.
+        :param arg: Argumentos que se pasaban a la funcion.
+        """ 
         t = (funcion, arg)
         self.funciones_normal.remove(t)
-    
-    def desconectar_presionado(self, funcion, arg = "null"):
+
+    def desconectar_presionado(self, funcion, arg="null"):
+        """ Elimina el método indicado asociado al estado presinado del botón.
+
+        :param funcion: Método al que se llama cuando el botón pasa estado Presionado.
+        :param arg: Argumentos que se pasaban a la funcion.
+        """ 
         t = (funcion, arg)
         self.funciones_press.remove(t)
-    
-    def desconectar_sobre(self, funcion, arg = "null"):
+
+    def desconectar_sobre(self, funcion, arg="null"):
+        """ Elimina el método indicado asociado al estado sobre del botón.
+
+        :param funcion: Método al que se llama cuando el botón pasa estado Sobre.
+        :param arg: Argumentos que se pasaban a la funcion.
+        """ 
         t = (funcion, arg)
-        self.funciones_over.remove(t) 
+        self.funciones_over.remove(t)
 
     def ejecutar_funciones_normal(self):
         if self.estado == True:
@@ -88,7 +152,7 @@ class Boton(Actor):
                     i[0]()
                 else:
                     i[0](i[1])
-    
+
     def ejecutar_funciones_press(self):
         if self.estado == True:
             for i in self.funciones_press:
@@ -96,8 +160,7 @@ class Boton(Actor):
                     i[0]()
                 else:
                     i[0](i[1])
-					
-    
+
     def ejecutar_funciones_over(self):
         if self.estado == True:
             for i in self.funciones_over:
@@ -109,24 +172,31 @@ class Boton(Actor):
     # funciones para inactivar o activar las funciones conectadas
     def activar(self):
         self.estado = True
-    
+
     def desactivar(self):
         self.estado = False
 
     # funciones que cambian la imagen del boton
     def pintar_normal(self):
+        """ Dibuja el botón en estado normal. """
         self.definir_imagen(self.imagen_normal)
 
-    def pintar_presionado(self, ruta_press = "null"):
+    def pintar_presionado(self, ruta_press="null"):
+        """ Dibuja el botón en estado presinado.
+
+        :param ruta_press: Opcional. Ruta de la imagen del boton presionado.
+        :type ruta_press: string
+        """
         if ruta_press == "null":
             self.imagen_press = pilas.imagenes.cargar(self.ruta_press)
         else:
-		    self.imagen_press = pilas.imagenes.cargar(ruta_press)
+            self.imagen_press = pilas.imagenes.cargar(ruta_press)
 
         self.definir_imagen(self.imagen_press)
 
     def pintar_sobre(self):
-        self.definir_imagen(self.imagen_over) 
+        """ Dibuja el botón en estado sobre. """
+        self.definir_imagen(self.imagen_over)
 
     def detection_move_mouse(self, evento):
         if self.colisiona_con_un_punto(evento.x, evento.y):
