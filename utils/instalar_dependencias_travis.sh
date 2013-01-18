098eee8f18755016598ae86e04aaf708452af905
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-01-18 19:28:14 -0300

    Actualizando ruta a los archivos tar.gz de qt para travis.ci

--- utils/instalar_dependencias_travis.sh
+++ utils/instalar_dependencias_travis.sh
@@ -7,17 +7,17 @@ sh -e /etc/init.d/xvfb start
 sudo apt-get update 
 sudo apt-get install g++ make python-dev libxext-dev
 sudo apt-get install libqt4-dev python-qt4 pyqt4-dev-tools qt4-designer python-qt4-gl python-qt4-phonon
-wget http://www.riverbankcomputing.co.uk/static/Downloads/sip4/sip-4.14.3-snapshot-ee3b1348996c.tar.gz
-tar zxf sip-4.14.3-snapshot-ee3b1348996c.tar.gz
-cd sip-4.14.3-snapshot-ee3b1348996c/
+wget http://thomasmansencal.com/Sharing/ThirdParty/Riverbank/sip-4.13.3/sip-4.13.3.tar.gz 
+tar zxf sip-4.13.3.tar.gz
+cd sip-4.13.3/
 python configure.py
 make
 sudo make install
 cd ..
 python -c 'import sip'
-wget http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-x11-gpl-snapshot-4.10-e143a1307289.tar.gz
-tar zxf PyQt-x11-gpl-snapshot-4.10-e143a1307289.tar.gz
-cd PyQt-x11-gpl-snapshot-4.10-e143a1307289
+wget http://thomasmansencal.com/Sharing/ThirdParty/Riverbank/PyQt%204.9.4/PyQt-x11-gpl-4.9.4.tar.gz
+tar zxf PyQt-x11-gpl-4.9.4.tar.gz
+cd PyQt-x11-gpl-4.9.4/
 echo 'yes' | python configure.py
 make
 sudo make install
