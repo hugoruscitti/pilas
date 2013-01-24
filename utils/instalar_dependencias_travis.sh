2b10660025cd7bc813a1fdcda7dfcbab5c748ace
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-01-24 08:55:52 -0300

    Probando instalar pyqt dentro del entorno virtual

--- utils/instalar_dependencias_travis.sh
+++ utils/instalar_dependencias_travis.sh
@@ -22,6 +22,29 @@ sudo apt-get install libqt4-dev python-qt4 pyqt4-dev-tools qt4-designer python-q
 #make -j 4
 #sudo make install
 #cd ..
+
+
+#!/bin/bash
+# This hook is run after a new virtualenv is activated.
+# ~/.virtualenvs/postmkvirtualenv
+ 
+libs=( PyQt4 sip.so )
+ 
+python_version=python$(python -c "import sys; print (str(sys.version_info[0])+'.'+str(sys.version_info[1]))")
+var=( $(which -a $python_version) )
+ 
+get_python_lib_cmd="from distutils.sysconfig import get_python_lib; print (get_python_lib())"
+lib_virtualenv_path=$(python -c "$get_python_lib_cmd")
+lib_system_path=$(${var[-1]} -c "$get_python_lib_cmd")
+ 
+for lib in ${libs[@]}
+do
+    ln -s $lib_system_path/$lib $lib_virtualenv_path/$lib 
+done
+
+
+
+
 python -c 'import PyQt4'
 python -c 'import PyQt4.QtCore'
 python -c 'import PyQt4.QtGui'
