2a3490844c45d0b419512e3f7f5dfbf4e6eb3e5d
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-01-24 00:35:44 -0300

    Documentando el actor Pausa.

--- pilas/actores/pausa.py
+++ pilas/actores/pausa.py
@@ -6,12 +6,17 @@
 #
 # Website - http://www.pilas-engine.com.ar
 
-import pilas
 from pilas.actores import Actor
 
 class Pausa(Actor):
+    """Representa un ícono que se mostrará cuando el juego esté en pausa."""
 
     def __init__(self, x=0, y=0):
+        """Inicia el actor de pausa.
+
+        :param x: Posición horizontal del ícono.
+        :param y: Posición vertical del ícono.
+        """
         Actor.__init__(self, x=x, y=y)
         self.centro = ('centro', 'centro')
         self.imagen = "icono_pausa.png"
