2a257347e010bb8e2a43b21214670a900b85d039
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-05-01 05:11:03 -0300

    pasando a la version 0.79

--- pilas/pilasversion.py
+++ pilas/pilasversion.py
@@ -17,39 +17,39 @@ Definición de la version actual de pilas y funciones para compararla.
 
 
 #: Contiene la versión actual de pilas.
-VERSION = "0.78"
+VERSION = "0.79"
 
 
 def compareactual(v):
     """Compara la versión actual de pilas con una que se pasa como parámetro
-    
+
     Sus posibles retornos son:
-    
+
     - **-1** si *versión actual de pilas* < ``v``.
     - **0** si *versión actual de pilas* == ``v``.
     - **1** si *versión actual de pilas* > ``v``.
-    
+
     :param v: versión a comparar con la actual.
     :type v: str
-    
+
     """
     return compare(VERSION, v)
 
 
 def compare(v0, v1):
     """Compara dos versiones de pilas.
-    
+
     Sus posibles retornos son
-    
+
     - **-1** si ``v0`` < ``v1``.
     - **0** si ``v0`` == ``v1``.
     - **1** si ``v0`` > ``v1``.
-    
+
     :param v0: primer versión a comparar.
     :type v0: str
     :param v1: segunda versión a comparar.
     :type v1: str
-    
+
     """
     v0 = v0.split(".")
     v1 = v1.split(".")
