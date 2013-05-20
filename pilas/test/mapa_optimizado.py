0acb13d0ff5fc66a81ff3c0aa17017b17ed55579
Quique Porta <quiqueporta@gmail.com>
2013-05-20 23:17:57 +0200

    Camdio del ejemplo de mapa

--- pilas/test/mapa_optimizado.py
+++ pilas/test/mapa_optimizado.py
@@ -2,28 +2,42 @@
 # Permite que este ejemplo funcion incluso si no has instalado pilas.
 import pilas
 
-def obtener_celda(evento):
-    global mapa
-    izquierda, derecha, arriba, abajo = pilas.escena_actual().camara.obtener_area_visible()
-
-    x, y = mapa.convertir_de_coordenada_absoluta_a_coordenada_mapa(izquierda, arriba)
-    y = mapa.obtener_numero_de_fila(y)
-    x = mapa.obtener_numero_de_columna(x)
-    print "X: %s" %(x)
-    print "Y: %s" %(y)
-
 def crear_mapa():
     mapa = pilas.actores.Mapa(filas=15, columnas=200)
 
+    # Plataforma superior (la que esta en medio de la pantalla)
+    mapa.pintar_bloque(9, 6, 0)
+    mapa.pintar_bloque(9, 7, 1)
+    mapa.pintar_bloque(9, 8, 1)
+    mapa.pintar_bloque(9, 9, 1)
+    mapa.pintar_bloque(9, 10, 1)
+    mapa.pintar_bloque(9, 11, 1)
+    mapa.pintar_bloque(9, 12, 2)
 
+    # Plataforma pequeña, mas abajo que la anterior.
+    mapa.pintar_bloque(12, 12, 0)
+    mapa.pintar_bloque(12, 13, 1)
+    mapa.pintar_bloque(12, 14, 1)
+    mapa.pintar_bloque(12, 15, 2)
+
+    mapa.pintar_bloque(13, 12, 8, False)
+    mapa.pintar_bloque(13, 13, 9, False)
+    mapa.pintar_bloque(13, 14, 9, False)
+    mapa.pintar_bloque(13, 15, 10, False)
+
+    mapa.pintar_bloque(13, 0, 7, True)
+    mapa.pintar_bloque(12, 0, 7, True)
+    mapa.pintar_bloque(11, 0, 7, True)
+    mapa.pintar_bloque(10, 0, 7, True)
+
+    mapa.pintar_bloque(13, 19, 7, True)
+    mapa.pintar_bloque(12, 19, 7, True)
+    mapa.pintar_bloque(11, 19, 7, True)
+    mapa.pintar_bloque(10, 19, 7, True)
 
     # Pinta todo el suelo
     for columna in range(0, 200):
         mapa.pintar_bloque(14, columna, 1)
-
-    # Pinta todo el suelo
-    for columna in range(0, 200):
-        mapa.pintar_bloque(9, columna, 1)
 
     return mapa
 
@@ -33,6 +47,5 @@ mapa = crear_mapa()
 martian = pilas.actores.Martian(mapa)
 martian.aprender(pilas.habilidades.SiempreEnElCentro)
 
-#pilas.escena_actual().mueve_mouse.conectar(obtener_celda)
 
 pilas.ejecutar()
