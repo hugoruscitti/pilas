1925d80fa3490d39e6e545237c34d4ce94cda82e
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-04-24 03:03:13 +0000

    Haciendo ajustes para armar paquete debian que funcione en ubuntu de 32 bits.

--- pilas/interprete.py
+++ pilas/interprete.py
@@ -186,8 +186,12 @@ class VentanaInterprete(Ui_InterpreteWindow):
                 ]
         else:
             codigo_inicial = []
-
-        consola = lanas.interprete.Ventana(self.splitter, scope, "\n".join(codigo_inicial))
+        
+        try:
+            consola = lanas.interprete.Ventana(self.splitter, scope, "\n".join(codigo_inicial))
+        except:
+            consola = lanas.lanas.interprete.Ventana(self.splitter, scope, "\n".join(codigo_inicial))
+        
         self.console.addWidget(consola)
         self.console.setCurrentWidget(consola)
         self.consola = consola
