f3fc211bf55e8571bda4b5707832badd1fedbcf3
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-05-02 03:03:57 -0300

    Cambiando el nombre de la propiedad para cambiar el zoom de la cámara.

--- pilasengine/escenas/camara.py
+++ pilasengine/escenas/camara.py
@@ -6,24 +6,24 @@ class Camara(object):
         self.escena = escena
         self._x = 0
         self._y = 0
-        self._aumento = 1
+        self._escala = 1
         self._rotacion = 0
 
     def aplicar_transformaciones_completas(self, painter):
         centro_x, centro_y = self.pilas.obtener_centro_fisico()
         painter.translate(centro_x, centro_y)
-        painter.scale(self._aumento, self._aumento)
+        painter.scale(self._escala, self._escala)
         painter.rotate(-self._rotacion)
 
     def aplicar_translacion(self, painter):
         centro_x, centro_y = self.pilas.obtener_centro_fisico()
         painter.translate(centro_x, centro_y)
 
-    def obtener_aumento(self):
-        return self._aumento
+    def obtener_escala(self):
+        return self._escala
 
-    def definir_aumento(self, valor):
-        self.pilas.utils.interpretar_propiedad_numerica(self, 'aumento', valor)
+    def definir_escala(self, valor):
+        self.pilas.utils.interpretar_propiedad_numerica(self, 'escala', valor)
 
     def obtener_rotacion(self):
         return self._rotacion
@@ -43,7 +43,7 @@ class Camara(object):
     def definir_y(self, valor):
         self.pilas.utils.interpretar_propiedad_numerica(self, 'y', valor)
 
-    aumento = property(obtener_aumento, definir_aumento, doc="Cambia el aumento o cercanía de la cámara.")
+    escala = property(obtener_escala, definir_escala, doc="Cambia el escala o cercanía de la cámara.")
     rotacion = property(obtener_rotacion, definir_rotacion, doc="Cambia la rotacion de la pantalla.")
     x = property(obtener_x, definir_x, doc="Cambia la posición x de la pantalla.")
     y = property(obtener_y, definir_y, doc="Cambia la posición y de la pantalla.")
