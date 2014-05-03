38b33d987291227343073b00311432bfd3ff4353
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-05-03 07:54:23 +0200

    creando archivos extra para compilar pilas-engine en windows.

---
+++ extras/cargador_windows/cargador.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+import tkMessageBox
+import Tkinter
+import imp
+import sys
+import os
+import pygame
+
+window = Tkinter.Tk()
+window.wm_withdraw()
+
+
+if not os.path.exists('ejecutar.py'):
+    tkMessageBox.showerror("Cargador de pilas-engine", "No se encuentra el archivo ejecutar.py")
+    sys.exit(1)
+
+try:
+    imp.load_source("__main__", "ejecutar.py")
+except Exception, e:
+    tkMessageBox.showerror("Error al ejecutar ejecutar.py", e)
+    sys.exit(1)
