28df44bad5407988d355f4918ce0cf67e1017e9b
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-05-13 00:03:32 -0300

    Agregando modo puntos de control.

--- pilasengine/depurador/modo_puntos_de_control.py
+++ pilasengine/depurador/modo_puntos_de_control.py
@@ -9,21 +9,10 @@ class ModoPuntosDeControl(ModoDepurador):
         ModoDepurador.__init__(self, pilas, depurador)
 
     def cuando_dibuja_actor(self, actor, painter):
+        self._definir_trazo_negro(painter)
+        painter.drawLine(-3, -3, 3, 3)
+        painter.drawLine(-3, 3, 3, -3)
+
         self._definir_trazo_blanco(painter)
-
-        x = "{0:0.1f}".format(actor.x)
-        y = "{0:0.1f}".format(actor.y)
-        texto = "(%s, %s)" %(x, y)
-
-        escala_x, escala_y = actor.escala_x, actor.escala_y
-
-        if actor._espejado:
-            escala_x *= -1
-
-        #painter.scale(1 -escala_x, 1-escala_y)
-        painter.rotate(actor.rotacion)
-
-        self._texto(painter, texto, 20, 20, color=blanco)
-
-        #painter.scale(escala_x, escala_y)
-        painter.rotate(-actor.rotacion)
+        painter.drawLine(-3, -3, 3, 3)
+        painter.drawLine(-3, 3, 3, -3)
