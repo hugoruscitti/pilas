08eccf3eed8377ccf1d7ed4b65eccd3d34705ec4
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-05-31 21:47:03 -0300

    Agregando numeración de lineas al editor.

--- pilasengine/interprete/editor.py
+++ pilasengine/interprete/editor.py
@@ -28,7 +28,134 @@ mono.rotacion = 0
 
 pilas.ejecutar()"""
 
-class Editor(autocomplete.CompletionTextEdit, editor_con_deslizador.EditorConDeslizador):
+
+
+
+
+from PyQt4.Qt import QFrame, QWidget, QTextEdit, QHBoxLayout, QPainter
+
+class Editor(QFrame):
+
+    class NumberBar(QWidget):
+
+        #def __init__(self, main, interpreterLocals, ventana_interprete, *args):
+        def __init__(self, *args):
+            QWidget.__init__(self, *args)
+            self.edit = None
+            # This is used to update the width of the control.
+            # It is the highest line that is currently visibile.
+            self.highest_line = 0
+
+        def setTextEdit(self, edit):
+            self.edit = edit
+
+        def update(self, *args):
+            '''
+            Updates the number bar to display the current set of numbers.
+            Also, adjusts the width of the number bar if necessary.
+            '''
+            # The + 4 is used to compensate for the current line being bold.
+            width = self.fontMetrics().width(str(self.highest_line)) + 4
+            if self.width() != width:
+                self.setFixedWidth(width)
+            QWidget.update(self, *args)
+
+        def paintEvent(self, event):
+            contents_y = self.edit.verticalScrollBar().value()
+            page_bottom = contents_y + self.edit.viewport().height()
+            font_metrics = self.fontMetrics()
+            current_block = self.edit.document().findBlock(self.edit.textCursor().position())
+
+            painter = QPainter(self)
+
+            line_count = 0
+            # Iterate over all text blocks in the document.
+            block = self.edit.document().begin()
+            while block.isValid():
+                line_count += 1
+
+                # The top left position of the block in the document
+                position = self.edit.document().documentLayout().blockBoundingRect(block).topLeft()
+
+                # Check if the position of the block is out side of the visible
+                # area.
+                if position.y() > page_bottom:
+                    break
+
+                # We want the line number for the selected line to be bold.
+                bold = False
+                if block == current_block:
+                    bold = True
+                    font = painter.font()
+                    font.setBold(True)
+                    painter.setFont(font)
+
+                # Draw the line number right justified at the y position of the
+                # line. 3 is a magic padding number. drawText(x, y, text).
+                painter.drawText(self.width() - font_metrics.width(str(line_count)) - 3, round(position.y()) - contents_y + font_metrics.ascent(), str(line_count))
+
+                # Remove the bold style if it was set previously.
+                if bold:
+                    font = painter.font()
+                    font.setBold(False)
+                    painter.setFont(font)
+
+                block = block.next()
+
+            self.highest_line = line_count
+            painter.end()
+
+            QWidget.paintEvent(self, event)
+
+
+    def __init__(self, main, interpreterLocals, ventana_interprete, *args):
+        QFrame.__init__(self, *args)
+
+        self.setFrameStyle(QFrame.StyledPanel | QFrame.Sunken)
+
+        self.editor = WidgetEditor(self, interpreterLocals, ventana_interprete)
+        self.editor.setFrameStyle(QFrame.NoFrame)
+        self.editor.setAcceptRichText(False)
+
+        self.number_bar = self.NumberBar()
+        self.number_bar.setTextEdit(self.editor)
+
+        hbox = QHBoxLayout(self)
+        hbox.setSpacing(0)
+        hbox.setMargin(0)
+        hbox.addWidget(self.number_bar)
+        hbox.addWidget(self.editor)
+
+        self.editor.installEventFilter(self)
+        self.editor.viewport().installEventFilter(self)
+
+    def eventFilter(self, object, event):
+        # Update the line numbers for all events on the text edit and the viewport.
+        # This is easier than connecting all necessary singals.
+        if object in (self.editor, self.editor.viewport()):
+            self.number_bar.update()
+            return False
+        return QFrame.eventFilter(object, event)
+
+    def actualizar_scope(self, scope):
+        self.editor.actualizar_scope(scope)
+
+    def definir_fuente(self, fuente):
+        self.editor.setFont(fuente)
+
+    def tiene_cambios_sin_guardar(self):
+        return self.editor.tiene_cambios_sin_guardar()
+
+    def ejecutar(self):
+        self.editor.ejecutar()
+
+    def document(self):
+        return self.editor.document()
+
+    def cargar_desde_archivo(self, ruta):
+        self.editor.cargar_desde_archivo(ruta)
+
+class WidgetEditor(autocomplete.CompletionTextEdit, editor_con_deslizador.EditorConDeslizador):
     """Representa el editor de texto que aparece en el panel derecho.
 
     El editor soporta autocompletado de código y resaltado de sintáxis.
