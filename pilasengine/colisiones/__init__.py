ad5f41ae138791956b43813df6f204e43853fd22
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-06-15 01:14:08 -0300

    Reparando interacción de grupos con colisiones.

--- pilasengine/colisiones/__init__.py
+++ pilasengine/colisiones/__init__.py
@@ -6,6 +6,8 @@
 #
 # Website - http://www.pilas-engine.com.ar
 
+import collections
+
 class Colisiones:
     "Administra todas las _colisiones entre actores."
 
@@ -62,148 +64,10 @@ class Colisiones:
     def agregar(self, grupo_a, grupo_b, funcion_a_llamar):
         "Agrega dos listas de actores para analizar _colisiones."
 
-        if not isinstance(grupo_a, list):
+        if not isinstance(grupo_a, list) and not isinstance(grupo_a, collections.MutableSequence):
             grupo_a = [grupo_a]
 
-        if not isinstance(grupo_b, list):
+        if not isinstance(grupo_b, list) and not isinstance(grupo_a, collections.MutableSequence):
             grupo_b = [grupo_b]
 
         self._colisiones_programadas_con_respuesta.append((grupo_a, grupo_b, funcion_a_llamar))
-
-    """
-    def verificar_colisiones(self):
-        print self._colisiones
-
-        # Analiza las colisiones físicas reportadas por box2d.
-        for info_colision in []:
-            actor_1 = info_colision['actor_1']
-            actor_2 = info_colision['actor_2']
-
-            # Luego recorre la lista de colisiones programadas, en busca
-            # de cualquier callback asociada a los dos actores que entraron
-            # en contacto.
-            for (grupo_a, grupo_b, funcion_a_llamar) in self._colisiones:
-
-                for a in grupo_a:
-                    for b in grupo_b:
-
-                        if a in (actor_1, actor_2) and b in (actor_1, actor_2):
-                            print "ASDASD"
-                            #funcion_a_llamar(a, b)
-
-        #self._colisiones_actuales = []
-
-
-        #for x in self._colisiones:
-        #    self._verificar_colisiones_en_tupla(x)
-        """
-
-    """
-    def _verificar_colisiones_en_tupla(self, tupla):
-        "Toma dos grupos de actores y analiza _colisiones entre ellos."
-        (grupo_a, grupo_b, funcion_a_llamar) = tupla
-
-        for a in grupo_a:
-            for b in grupo_b:
-                try:
-                    if id(a) != id(b) and utils.colisionan(a, b):
-                        funcion_a_llamar(a, b)
-
-                    # verifica si alguno de los dos objetos muere en la colision.
-                    if a not in self.pilas.escena_actual().actores:
-                        if a in grupo_a:
-                            list.remove(grupo_a, a)
-
-                    if b not in self.pilas.escena_actual().actores:
-                        if b in grupo_b:
-                            list.remove(grupo_b, b)
-                except Exception as e:
-                    list.remove(grupo_a, a)
-                    raise e
-
-    def verificar_colisiones_fisicas(self, id_actor_a, id_actor_b):
-        for x in self._colisiones:
-            self._verificar_colisiones_fisicas_en_tupla(x, id_actor_a, id_actor_b)
-
-    def _verificar_colisiones_fisicas_en_tupla(self, tupla, id_actor_a, id_actor_b):
-        "Toma dos grupos de actores y analiza _colisiones entre ellos."
-        (grupo_a, grupo_b, funcion_a_llamar) = tupla
-
-        for a in grupo_a:
-            for b in grupo_b:
-                try:
-                    if self._es_objeto_fisico_con_actor_asociado(a):
-                        a_id = a.figura.id
-                    else:
-                        a_id = a.id
-
-                    if self._es_objeto_fisico_con_actor_asociado(b):
-                        b_id = b.figura.id
-                    else:
-                        b_id = b.id
-
-                    if a_id == id_actor_a and b_id == id_actor_b:
-                        funcion_a_llamar(a, b)
-
-                        # verifica si alguno de los dos objetos muere en la colision.
-                        if (self._es_objeto_fisico_con_actor_asociado(a)):
-                            if a not in pilas.escena_actual().actores:
-                                if a in grupo_a:
-                                    list.remove(grupo_a, a)
-
-                        if (self._es_objeto_fisico_con_actor_asociado(b)):
-                            if b not in pilas.escena_actual().actores:
-                                if b in grupo_b:
-                                    list.remove(grupo_b, b)
-
-                except Exception as e:
-                    list.remove(grupo_a, a)
-                    raise e
-
-
-    def _es_objeto_fisico_con_actor_asociado(self, objeto):
-        # Comprobamos si el objeto tiene la propiedad "figura" establecida.
-        # Esta propiedad se establece en la Habilidad de Imitar.
-        return hasattr(objeto, 'figura')
-    """
-
-
-    """
-    def eliminar_colisiones_con_actor(self, actor):
-
-        for x in self._colisiones:
-
-            grupo_a = x[0]
-            grupo_b = x[1]
-            #funcion_a_llamar = x[2]
-
-            if actor in grupo_a:
-                # Si solo estaba el actor en este grupo eliminamos la colision.
-                if len(grupo_a) == 1:
-                    self._colisiones.remove(x)
-                else:
-                    # Si hay mas de un actore eliminamos el actor de la lista.
-                    grupo_a.remove(x)
-                break
-
-            if actor in grupo_b:
-                # Si solo estaba el actor en este grupo eliminamos la colision.
-                if len(grupo_b) == 1:
-                    self._colisiones.remove(x)
-                else:
-                    # Si hay mas de un actore eliminamos el actor de la lista.
-                    grupo_b.remove(x)
-                break
-
-    def obtener_colisiones(self, actor, grupo_de_actores):
-        "Retorna una lista de los actores que colisionan con uno en particular."
-
-        lista_de_colisiones = []
-
-        for a in grupo_de_actores:
-            if id(actor) != id(a) and utils.colisionan(actor, a):
-                lista_de_colisiones.append(a)
-
-        return lista_de_colisiones
-
-    """
