166a624ac7627e15843ce73f62fa548e6851288f
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-08-21 13:02:12 -0300

    normalizando numero de version

--- setup-mac.py
+++ setup-mac.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='pilas-engine',
+    version="0.90.0",
     app=["bin/pilasengine"],
     data_files = [
         ('bin/pilasengine', ['bin/pilasengine']),
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(
         name='pilas',
         zip_safe=False,
-        version="0.9",
+        version="0.90.0",
         description="""============
 Pilas Engine
 ============
