c8d84ccaa6858d12865eada6bd947babd35e12fb
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-03-09 16:06:38 -0300

    quitando ejemplos con errores.

--- pilasengine/ejemplos/aceitunas_vs_bombas_paso_1.py
+++
@@ -1,8 +0,0 @@
-import pilasengine
-
-pilas = pilasengine.iniciar()
-protagonista = pilas.actores.Aceituna()
-protagonista.aprender(pilas.habilidades.SeguirAlMouse)
-pilas.ocultar_puntero_del_mouse()
-
-pilas.ejecutar()
