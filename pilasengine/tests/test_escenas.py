1c3eb76610fdf914137f5df3e98f8a5b191c82a8
Horacio G. de Oro <hgdeoro@gmail.com>
2015-05-19 13:23:39 -0300

    Elimina import innecesario realizado en metodo

--- pilasengine/tests/test_escenas.py
+++ pilasengine/tests/test_escenas.py
@@ -1,7 +1,9 @@
 # -*- encoding: utf-8 -*-
 import sys
 import unittest
+
 from PyQt4 import QtGui
+
 import pilasengine
 
 
@@ -9,7 +11,6 @@ class TestEscenas(unittest.TestCase):
     app = QtGui.QApplication(sys.argv)
 
     def setUp(self):
-        import pilasengine
         self.pilas = pilasengine.iniciar()
 
     def testPuedeCrearVincularUnaEscena(self):
