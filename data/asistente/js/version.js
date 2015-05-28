7a6386d03715f72fe515387a81ddae46d26b28b2
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-05-28 16:20:02 -0300

    release 0.90.34

--- data/asistente/js/version.js
+++ data/asistente/js/version.js
@@ -1,7 +1,7 @@
 
 window.DESCRIPCION_VERSION = {
-  version: '0.90.33',
-  fecha: '23 de May del 2015',
-  commit: 'bd08464',
-  changelog: ['actualizando manual.', 'release 0.90.32', 'Reparando el bug que hacia fallar las interpolaciones con 0.', 'Agregando el comando log a makefile.', 'actualizando manual.', ]
+  version: '0.90.34',
+  fecha: '28 de May del 2015',
+  commit: '8813480',
+  changelog: ['actualizando manual.', 'asegurandose de generar siempre archivos .py desde el editor al guardar.', 'merge', 'Agregando selector de archivos y un nuevo botón para guardar el código del editor.', 'release 0.90.33', ]
 }
