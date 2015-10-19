7d396f55ed2bae54ae65a9e8ffb55097d23e9cf3
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-10-19 02:00:25 -0300

    update

--- assets/pilas-website-test.js
+++ assets/pilas-website-test.js
@@ -1422,10 +1422,10 @@ define('pilas-website-test/routes/descargas', ['exports', 'ember'], function (ex
   exports['default'] = Ember['default'].Route.extend({
     model: function () {
       return {
-        version: "1.2.0",
-        fecha: "2015-09-26",
-        link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/1.2.0/pilas-engine-1.2.0.dmg",
-        link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/1.2.0/pilas-engine_1.2.0.exe",
+        version: "1.3.0",
+        fecha: "2015-10-19",
+        link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/1.3.0/pilas-engine-1.3.0.dmg",
+        link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/1.3.0/pilas-engine_1.3.0.exe",
         link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/python-pilas/" };
     }
   });
